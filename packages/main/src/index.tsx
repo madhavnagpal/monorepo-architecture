bd11a3a395ac72eccffc04f037ce79af70168a9d
Madhav <mr.madhavnagpal@gmail.com>
2023-06-20 21:26:38 +0530

    imported components from team-b

--- packages/main/src/index.tsx
+++ packages/main/src/index.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { App } from 'team-a';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { App } from "team-a";
+import { AppFromTeamB } from "team-b";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <App />
+    <AppFromTeamB />
   </React.StrictMode>
 );
 
